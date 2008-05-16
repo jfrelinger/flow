5363d978b49091b4a2303109a7757cf095441972
cliburn@lister.dulci.duhs.duke.edu <cliburn@lister.dulci.duhs.duke.edu>
2008-05-16 19:28:46 -0400

    Add reviewer pdfs and interpolation for pcolor2

--- src/experimental/pcolor2.py
+++ src/experimental/pcolor2.py
@@ -13,17 +13,35 @@ yu = numpy.random.uniform(-5, 5, 1000)
 x = numpy.concatenate([xn1, xn2, xu])
 y = numpy.concatenate([yn1, yn2, yu])
 
-bins = 250
+bins = 100
 z, xedge, yedge = numpy.histogram2d(y, x, bins=[bins, bins], 
                                     range=[(numpy.min(y), numpy.max(y)),
                                            (numpy.min(x), numpy.max(x))])
 
-xint = map(int, (x - numpy.min(x))/(numpy.max(x)-numpy.min(x))*(bins-1))
-yint = map(int, (y - numpy.min(y))/(numpy.max(y)-numpy.min(y))*(bins-1))
+# interpolate to get rid of blocky effect
+xfrac, xint = numpy.modf((x - numpy.min(x))/
+                         (numpy.max(x)-numpy.min(x))*(bins-1))
+yfrac, yint = numpy.modf((y - numpy.min(y))/
+                         (numpy.max(y)-numpy.min(y))*(bins-1))
 
-zvals = numpy.array([z[_y, _x] for _x, _y in zip(xint, yint)])
+zvals = numpy.zeros(len(xint), 'd')
+for i, (_x, _y, _xf, _yf) in enumerate(zip(xint, yint, xfrac, yfrac)):
+    q11 = z[_y, _x]
+    if _xf:
+        q12 = z[_y, _x+1]
+    else:
+        q12 = 0
+    if _yf:
+        q21 = z[_y+1, _x]
+    else:
+        q21 = 0
+    if _xf and _yf:
+        q22 = z[_y+1, _x+1]
+    else:
+        q22 = 0
 
-print max(xint), max(yint)
+    zvals[i] = q11*(1-_xf)*(1-_yf) + q21*(1-_xf)*(_yf) + \
+        q12*(_xf)*(1-_yf) + q22*(_xf)*(_yf)
 
 ax = pylab.subplot(111)
 ax.scatter(x, y, s=1, c=zvals, faceted=False)
