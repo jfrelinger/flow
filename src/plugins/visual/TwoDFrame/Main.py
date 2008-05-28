9cdb53666d638b848ac5ad5f8e22b636f3c18c2b
jolly <none@none>
2008-05-28 15:01:06 -0400

    If no colors are defined, estimate and color by density in 2d scatter plot.

--- src/plugins/visual/TwoDFrame/Main.py
+++ src/plugins/visual/TwoDFrame/Main.py
@@ -2,7 +2,7 @@ import wx
 
 from plots import PlotPanel
 import densities2 as dens
-from numpy import array, arange, mgrid, isnan, sqrt, histogram2d, min, max, take
+from numpy import array, arange, mgrid, isnan, sqrt, histogram2d, min, max, take, modf
 from numpy.linalg import inv
 from numpy.random import randn, rand
 from pylab import cm, hist, zeros
@@ -378,7 +378,38 @@ class TwoDPanel(PlotPanel):
                         yy = self.y[z==i][:10000*stride:stride]
                         self.subplot.plot(xx, yy, '.', c=self.colors[i], alpha=alpha, ms=self.ms)
                 else:
-                    s = self.subplot.plot(x, y, '.', alpha=alpha, ms=self.ms)
+                    bins = 100
+                    z, xedge, yedge = histogram2d(y, x, bins=[bins, bins], 
+                                                        range=[(min(y), max(y)),
+                                                               (min(x), max(x))])
+                    
+                    # interpolate to get rid of blocky effect
+                    # from http://en.wikipedia.org/wiki/Bilinear_interpolation
+                    xfrac, xint = modf((x - min(x))/
+                                             (max(x)-min(x))*(bins-1))
+                    yfrac, yint = modf((y - min(y))/
+                                             (max(y)-min(y))*(bins-1))
+                    
+                    zvals = zeros(len(xint), 'd')
+                    for i, (_x, _y, _xf, _yf) in enumerate(zip(xint, yint, xfrac, yfrac)):
+                        q11 = z[_y, _x]
+                        if _xf:
+                            q12 = z[_y, _x+1]
+                        else:
+                            q12 = 0
+                        if _yf:
+                            q21 = z[_y+1, _x]
+                        else:
+                            q21 = 0
+                        if _xf and _yf:
+                            q22 = z[_y+1, _x+1]
+                        else:
+                            q22 = 0
+                    
+                        zvals[i] = q11*(1-_xf)*(1-_yf) + q21*(1-_xf)*(_yf) + \
+                            q12*(_xf)*(1-_yf) + q22*(_xf)*(_yf)
+
+                    s = self.subplot.scatter(x, y, alpha=alpha, s=1, c=zvals, faceted=False )
             alpha = alpha - .25
         if self.contour.IsChecked() :
             bins = int(0.25*sqrt(len(self.x)))
