03178a78f43c60107a595f87420a37862d2376eb
jolly@jolly-ubutnu-emulation <jolly@jolly-ubutnu-emulation>
2008-06-09 15:03:35 -0400

    more work on packagebuilding

--- packaging/build.sh
+++ packaging/build.sh
@@ -4,6 +4,10 @@ cd ../src && find . -name '*.py' | cpio -pvmud ../packaging/debian/usr/lib/pytho
 cd -
 mkdir -p debian/usr/lib/python2.5/site-packages/flow/obo
 cp ../src/obo/cell.obo debian/usr/lib/python2.5/site-packages/flow/obo
+mkdir -p debian/usr/bin/
+echo '#!/bin/sh' > debian/usr/bin/flow
+echo 'cd /usr/lib/python2.5/site-packages/flow && /usr/bin/python2.5  /usr/lib/python2.5/site-packages/flow/App.py' >> debian/usr/bin/flow
+
 echo 'copy bayes.so to debian/usr/lib/python2.5/site-packages/flow/plugins/statistics/Bayes/'
 echo 'copy flow.so to debian/usr/lib/python2.5/site-packages/flow/plugins/statistics/Kde/'
 echo 'then run dpkg-deb --build debian'
