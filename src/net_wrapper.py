2b51e17248e8589c4258d068e2e600112a56a9b4
jolly <none@none>
2008-07-28 15:41:52 -0400

    fill in some of wrapper code.

--- src/net_wrapper.py
+++ src/net_wrapper.py
@@ -1,21 +1,28 @@
+import xmlrpclib
 '''
 network layer wrapper for xmlrpc code
 '''
 
-class Session_manager(object):
-    def __init__(self):
-        pass
-    def send_data(self, array):
-        pass
-    def send_job(self, job_def):
-        pass
-    def get_status(self, job_id):
-        pass
-    def get_result(self, job_id):
-        pass
 
-def connect():
+class Session_manager(object):
+    def __init__(self, url):
+        self.server = xmlrpclib.Server(url)
+        
+    def send_data(self, array):
+        self.server.send_data(array)
+        
+    def send_job(self, job_def):
+        self.server.send_job(job_def)
+        
+    def get_status(self, job_id):
+        return self.server.get_status(job_id)
+        
+    def get_result(self, job_id):
+        return self.server.get_result(job_id)
+
+def connect(url, username, pw):
     '''
     eventually will setup a newwork connection and return the session_manager object
     '''
-    return Session_manager()
+    
+    return Session_manager(url)
