a928d64f5e3c5194cbe2d8367422dc99895d889f
jolly <none@none>
2008-07-28 16:17:46 -0400

    more work on the client side.

--- src/net_wrapper.py
+++ src/net_wrapper.py
@@ -6,19 +6,26 @@ network layer wrapper for xmlrpc code
 
 class Session_manager(object):
     def __init__(self, url):
-        self.server = xmlrpclib.Server(url)
-        
+        self.session = xmlrpclib.Server(url)
+        print self.session
+        #self.user = user
+        #self.pw = pw
+
     def send_data(self, array):
-        self.server.send_data(array)
+        self.data = array
+        #self.session.send_data(array)
         
     def send_job(self, job_def):
-        self.server.send_job(job_def)
+        self.session.send_job(job_def)
         
     def get_status(self, job_id):
-        return self.server.get_status(job_id)
+        return self.session.get_status(job_id)
         
     def get_result(self, job_id):
-        return self.server.get_result(job_id)
+        return self.session.get_result(job_id)
+    
+    def get_server_status(self):
+        return self.session.server_status()
 
 def connect(url, username, pw):
     '''
@@ -26,3 +33,7 @@ def connect(url, username, pw):
     '''
     
     return Session_manager(url)
+
+if __name__ == '__main__':
+    session = connect('http://localhost:8000', 'foo', 'bar')
+    print session.get_server_status()
