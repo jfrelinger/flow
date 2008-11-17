d315e9dc2879bb505f2bfdc5d8e19bec89fae42c
jolly <none@none>
2008-11-17 09:21:01 -0500

    new versions of matplotlib complain on using facedted=False and now want edges='none'. this is a commit of a serarch and replace that updates this.

--- src/plugins/visual/TwoDFrame/Main.py
+++ src/plugins/visual/TwoDFrame/Main.py
@@ -530,7 +530,7 @@ class TwoDPanel(PlotPanel):
                         zvals[i] = q11*(1-_xf)*(1-_yf) + q21*(1-_xf)*(_yf) + \
                             q12*(_xf)*(1-_yf) + q22*(_xf)*(_yf)
 
-                    s = self.subplot.scatter(x, y, alpha=alpha, s=1, c=zvals, faceted=False )
+                    s = self.subplot.scatter(x, y, alpha=alpha, s=1, c=zvals, edgecolors='none' )
             alpha = alpha - .25
 
         # always put labels on if possible at mean location
